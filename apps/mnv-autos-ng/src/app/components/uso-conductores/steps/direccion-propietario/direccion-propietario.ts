e09098250cca4602f312f457d03c99fa14324ec1
Soufiane Asrih <soufasri@emeal.nttdata.com>
2026-07-09 00:38:17 +0100

    translate

--- apps/mnv-autos-ng/src/app/components/uso-conductores/steps/direccion-propietario/direccion-propietario.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-direccion-propietario',
-  standalone: true,
-  templateUrl: './direccion-propietario.html',
-  styleUrls: ['./direccion-propietario.scss'],
-})
-export class DireccionPropietarioComponent {}
